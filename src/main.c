581721f3cdc3680ef558b1f038f478b36881bb56
Finlay Maguire <finlaymaguire@gmail.com>
2015-06-24 19:45:35 +0100

    Implement simple vm capable of stack addition

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include<stdbool.h>
+#include<stdio.h>
 
 // Enumerator to make readable instruction names
 typedef enum {
@@ -9,6 +11,7 @@ typedef enum {
 } InstructionSet;
 
 // Simple program adding two numbers in stack
+// implemented as an array to be iterated over
 const int program[] = {
     PSH, 2,
     PSH, 2,
@@ -17,7 +20,50 @@ const int program[] = {
     HLT
 };
 
+// Init instruction pointer/program counter
+int ip = 0;
+int sp = -1;
+int stack[256];
+
+// Preventing infinite loop running
+bool running = true;
+
+int fetch(){
+    return program[ip];
+}
+
+void eval(int instr) {
+    switch (instr) {
+        case HLT: {
+            running = false;
+            break;
+        }
+        case PSH: {
+            sp++;
+            stack[sp] = program[++ip];
+            break;
+        }
+        case POP: {
+            int val_popped = stack[sp--];
+            printf("popped %d\n", val_popped);
+            break;
+        }
+        case ADD: {
+            int a = stack[sp--];
+            int b = stack[sp--];
+            int result = a+b;
+            sp++;
+            stack[sp] = result;
+            break;
+        }
+    }
+}
 
 int main() {
-    return 0;
+    while(running) {
+        eval(fetch());
+        ip++;
+    }
+
+   return 0;
 }
