ee773b545936ff89190887cb98f5520e6ffb5bd4
Finlay Maguire <finlaymaguire@gmail.com>
2015-06-24 19:33:17 +0100

    Add typedefs for vm instructions and a simple program

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
+
+// Enumerator to make readable instruction names
+typedef enum {
+    PSH,
+    ADD,
+    POP,
+    SET,
+    HLT
+} InstructionSet;
+
+// Simple program adding two numbers in stack
+const int program[] = {
+    PSH, 2,
+    PSH, 2,
+    ADD,
+    POP,
+    HLT
+};
+
+
 int main() {
     return 0;
 }
